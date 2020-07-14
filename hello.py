5ddc9e62486f7d8c71e2e6472f7d9dc40320b72f
Tony <tony.g.kay@gmail.com>
2020-07-14 05:16:40 -0600

    Initial work with templates

--- hello.py
+++ hello.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python
 
-from flask import Flask, request
-from flask import redirect
+from flask import Flask, redirect, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World</h1>', 200
+    return render_template('index.html')
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello is {}</h1>'.format(name)
+    return render_template('user.html', name=name)
+#    return '<h1>Hello is {}</h1>'.format(name)
 
 @app.route('/user/<int:id>')
 def id(id):
