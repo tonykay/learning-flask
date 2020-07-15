c28d15d93b7f696e88b14b47ebc1edf22d778bf4
Tony <tony.g.kay@gmail.com>
2020-07-15 11:50:49 -0600

    Enhanced database form up to 5b

--- hello.py
+++ hello.py
@@ -53,12 +53,20 @@ class NameForm(FlaskForm):
 def form():
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-            flash('Looks like you have changed your name!')
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            db.session.commit()
+            session['known'] = False
+        else:
+            session['known'] = True
         session['name'] = form.name.data
+        form.name.data = ''
         return redirect(url_for('form'))
-    return render_template('first_form.html', form=form, name=session.get('name'))
+    return render_template('first_form.html',
+        site=site, form=form, name=session.get('name'),
+        known=session.get('known', False))
 
 
 @app.route('/')                         # Basic view function
