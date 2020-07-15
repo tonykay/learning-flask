93f208515d3565e8496e8301ee84d3ce0c03a09a
Tony <tony.g.kay@gmail.com>
2020-07-15 11:39:51 -0600

    Initial database work using sqlite

--- hello.py
+++ hello.py
@@ -3,12 +3,43 @@
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
+from sqlalchemy.sql.schema import PrimaryKeyConstraint
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+import os
 
+# setup for sqlite
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+
 app.config['SECRET_KEY'] = 'r3dh4t1!'   # For now, externalize later
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role %r' % self.name
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 bootstrap = Bootstrap(app)              # Setting up bootstrap
 
@@ -29,7 +60,8 @@ def form():
         return redirect(url_for('form'))
     return render_template('first_form.html', form=form, name=session.get('name'))
 
-@app.route('/')
+
+@app.route('/')                         # Basic view function
 def index():
     return render_template('index.html', site=site)
 
