c65b994d54bf5fd28434588d503a85171f5ced3f
Tony <tony.g.kay@gmail.com>
2020-07-16 13:13:11 -0600

    Playing with config database population

--- config.py
+++ config.py
@@ -1,14 +1,18 @@
-from hello import db
+from hello import db, Role, User
+
 db.create_all()
-from hello import Role, User
+
 admin_role = Role(name='Admin')
 mod_role = Role(name='Moderator')
 user_role = Role(name='User')
 user_john = User(username='peter', role=admin_role)
 user_susan = User(username='maja', role=user_role)
+user_tony = User(username='tony', role=user_role)
+db.session.add(Role(name='foobar'))
 db.session.add(admin_role)
 db.session.add(mod_role)
 db.session.add(user_role)
 db.session.add(user_john)
 db.session.add(user_susan)
+db.session.add(user_tony)
 db.session.commit()
